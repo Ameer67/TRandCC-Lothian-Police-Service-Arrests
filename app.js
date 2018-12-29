41a456f8c0b28e2793f777aacce2bfbd2c69e15a
Ameer67 <ameerzaita9@gmail.com>
2018-12-28 20:05:05 -0600

    added blur to container

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-let firstName = document.querySelector('input#first-name');
+// let mainContainerHeight = getComputedStyle(document.querySelector('#main-container')).height;
+// let mainContainerBeforeHeight = getComputedStyle(document.querySelector('#main-container'), '::before');
 
-firstName.addEventListener('focus', function(e){
-    firstName.value = '';
-    console.log(e);
-})
+// let styleSheet = document.styleSheets[0];
+// styleSheet.insertRule(`#main-container::before {
+//     height: ${mainContainerHeight} !important;
+// }`, 1);
 
-firstName.addEventListener('blur', function(e){
-    if(firstName.value === ''){
-        firstName.value = 'test';
-    }
+
+// let firstName = document.querySelector('input#first-name');
+
+// firstName.addEventListener('focus', function(e){
+//     firstName.value = '';
+//     console.log(e);
+// })
+
+// firstName.addEventListener('blur', function(e){
+//     if(firstName.value === ''){
+//         firstName.value = 'test';
+//     }
     
-    console.log(e);
-})
+//     console.log(e);
+// })
