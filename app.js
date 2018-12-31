7cc4ee20e2c7a65bc091d0c18d5cd32d11466260
Ameer67 <ameerzaita9@gmail.com>
2018-12-31 16:20:03 -0600

    changed arrestFormFunction to output date in different format

--- app.js
+++ app.js
@@ -19,17 +19,22 @@ function arrestFormFunction(e){
 
         if (allInputsFilled === true) { // do the following if all inputs are filled
 
+            let arrestDate = new Date(arrestForm[0].value + ' '); // change arrest date into date object
+            let monthDay = arrestDate.toDateString().substring(4); // convert to string and remove day name 
+            let year = monthDay.slice(6, 11); // grab year
+            monthDay = monthDay.slice(0, 6) + ','; // grab month and add ,
+            
             let newRecord = document.createElement('li');
             newRecord.className = 'arrest';
             newRecord.innerHTML = `
                 <div class="arrest-info">
-                    <div class="arrest-date"> ${arrestForm[0].value}</div>
+                    <div class="arrest-date"> ${monthDay + year}</div>
                     <div class="arrest-name"> ${arrestForm[1].value} </div>
                     <div class="arrest-car"> ${arrestForm[2].value} ${arrestForm[3].value} ${arrestForm[4].value}</div>
                 </div>
                 <a href="#" class="delete-arrest"><img src="img/x.png"></a>
             `;
-
+            
             arrestLog.push(newRecord); // add the new element to the array
             document.querySelector('.no-results').style.display = 'none'; // hide 'no results'
             for (let i = 0; i < arrestForm.length - 1; i++){ // clear inputs
